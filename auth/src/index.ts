aa2014cd8846180f4f4880e5d9aaea7857a00f43
vvduth <ducthai060501@gmail.com>
2024-12-29 14:54:00 +0200

    Auth serive set up

---
+++ auth/src/index.ts
@@ -0,0 +1,18 @@
+import express, { Application, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+
+const app: Application = express();
+const port: number = 3000;
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+
+// Basic route
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, world!');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
