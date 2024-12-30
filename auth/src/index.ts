2f433f2f6dac15ac955b1ea755970ac60e56e21f
vvduth <ducthai060501@gmail.com>
2024-12-30 13:58:35 +0200

    Add MongoDB integration and deployment configuration for authentication service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,13 @@
-import express, { Application, Request, Response } from 'express';
-import bodyParser from 'body-parser';
-import { currentUserRouter } from './routes/current-user';
-import { signUprouter } from './routes/signup';
-import { singInRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-import 'express-async-errors'
+import express, { Application, Request, Response } from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import { currentUserRouter } from "./routes/current-user";
+import { signUprouter } from "./routes/signup";
+import { singInRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+import "express-async-errors";
 
 const app: Application = express();
 const port: number = 3000;
@@ -14,21 +15,31 @@ const port: number = 3000;
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
 
-
 // Basic route
-app.use(currentUserRouter)
-app.use(signUprouter)
-app.use(singInRouter)
-app.use(signoutRouter)
+app.use(currentUserRouter);
+app.use(signUprouter);
+app.use(singInRouter);
+app.use(signoutRouter);
 
-app.all('*',  () => {
-    throw new NotFoundError();
-})
+app.all("*", () => {
+  throw new NotFoundError();
+});
 
 app.use(errorHandler as express.ErrorRequestHandler);
 
+const start = async () => {
+  try {
+    mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error(error);
+  }
 
-// Start the server
-app.listen(port, () => {
+  // Start the server
+  app.listen(port, () => {
     console.log(`Server is running  on http://localhost:${port}`);
-});
+  });
+};
+
+
+start();
