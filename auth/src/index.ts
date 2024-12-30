05ea3192c81738271c94684a828b5e84df06bd63
vvduth <ducthai060501@gmail.com>
2024-12-30 16:44:33 +0200

    Add cookie-session dependency and configure session middleware in the application

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express, { Application, Request, Response } from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
+
 import { currentUserRouter } from "./routes/current-user";
 import { signupRouter } from "./routes/signup";
 import { singInRouter } from "./routes/signin";
@@ -10,10 +12,12 @@ import { NotFoundError } from "./errors/not-found-error";
 import "express-async-errors";
 
 const app: Application = express();
+app.set("trust proxy", true);
 const port: number = 3000;
 
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
+app.use(cookieSession({ signed: false, secure: true }));
 
 // Basic route
 app.use(currentUserRouter);
