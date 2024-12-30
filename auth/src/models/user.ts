94966a8385281fd23737ef212787a89efd89e0a6
vvduth <ducthai060501@gmail.com>
2024-12-30 14:25:36 +0200

    Refactor User model interfaces to improve type safety and clarity

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -8,8 +8,14 @@ interface UserAttrs {
 }
 
 // interface that describes the properties that a User Model has
-interface UserModel extends mongoose.Model<any> {
-    build(attrs: UserAttrs): any;
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attrs: UserAttrs): UserDoc;
+}
+
+// interface that describes the properties that a User Document has
+interface UserDoc extends mongoose.Document {
+    email: string;
+    password: string;
 }
 
 const userSchema = new mongoose.Schema({
@@ -27,10 +33,6 @@ userSchema.statics.build = (attrs: UserAttrs) => {
     return new User(attrs);
 }
 
-const User = mongoose.model<any, UserModel>('User', userSchema);
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
-User.build({
-    email: 'test@gmail.com', 
-    password: 'password'
-})
 export { User };
