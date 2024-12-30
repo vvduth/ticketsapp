3eb6f7381953f87ef4c36d2ea880314f9565e3e5
vvduth <ducthai060501@gmail.com>
2024-12-30 10:25:45 +0200

    Refactor error handling to include status codes and improve error serialization for database connection and validation errors

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,9 +1,14 @@
 export class DataBaseConnectionError extends Error {
 
+    statusCode = 500;
     reason:string = "Error connecting to database";
     constructor() {
         super();
         // Only because we are extending a built in class
         Object.setPrototypeOf(this, DataBaseConnectionError.prototype);
+    }
+
+    serizalizeErrors() {
+        return [{ message: this.reason}];
     }
 }
--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,27 +1,21 @@
-import { NextFunction, Request, Response } from "express";
+import e, { NextFunction, Request, Response } from "express";
 import { RequestValidationError } from "../errors/request-validation-error";
 import { DataBaseConnectionError } from "../errors/database-connection-error";
 
 export const errorHandler = (
-    err: Error,
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
-    if (err instanceof RequestValidationError) {
-      const formattedErrors = err.errors.map((error) => {
-        if (error.type === 'field') {
-          return { message: error.msg, field: error.path };
-        }
-      });
-      return res.status(400).send({ errors: formattedErrors });
-    }
-    if (err instanceof DataBaseConnectionError) {
-      return res.status(500).send({ errors: [{ message: err.reason }] });
-    }
-  
-    res.status(400).send({
-      errors: [{ message: 'Something went wrong' }],
-    });
-  };
-  
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof RequestValidationError) {
+    return res.status(400).send({ errors: err.serizalizeErrors() });
+  }
+  if (err instanceof DataBaseConnectionError) {
+    return res.status(500).send({ errors: err.serizalizeErrors() });
+  }
+
+  res.status(400).send({
+    errors: [{ message: "Something went wrong" }],
+  });
+};
