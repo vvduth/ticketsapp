e57d11275da49dadcb575579c4322cc82b3f6e01
vvduth <ducthai060501@gmail.com>
2024-12-31 09:06:01 +0200

    Add request validation middleware and refactor signin/signup routes

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,11 +1,10 @@
 import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
-import { RequestValidationError } from "../errors/request-validation-error";
-
+import { body } from "express-validator";
+import { validateRequest } from "../middlewares/validate-request";
 const router = express.Router();
 
 router.post(
-    "/api/users/singin",
+    "/api/users/signin",
   [
     body("email").isEmail().withMessage("Email must be valid"),
     body("password")
@@ -13,12 +12,9 @@ router.post(
       .notEmpty()
       .withMessage("You must supply a password"),
   ],
+  validateRequest,
   (req: Request, res: Response) => {
-    console.log("singin route");
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
+    
 
   }
 );
