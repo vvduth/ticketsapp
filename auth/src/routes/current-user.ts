82c10f0ad8bfd548a015733473897fbf8ebba304
vvduth <ducthai060501@gmail.com>
2024-12-31 09:51:38 +0200

    Add currentUser middleware to extract user info from JWT; refactor current user route to utilize middleware and simplify response; fix typo in signout route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,21 +1,13 @@
 import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
-
+import { currentUser } from "../middlewares/current-user";
 const router = express.Router();
 
 router.get(
   "/api/users/currentuser",
+  currentUser,
   (req: Request, res: Response): any | Promise<any> => {
-    if (!req.session?.jwt) {
-      return res.send({ currentUser: null });
-    }
-
-    try {
-      const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-      res.send({ currentUser: payload });
-    } catch (error) {
-      res.send({ currentUser: null });
-    }
+    res.send({ currentUser: req.currentUser || null });
   }
 );
 
