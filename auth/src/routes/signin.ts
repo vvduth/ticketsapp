f2fb454b106aa53846557ce1cac4dc69c2720198
vvduth <ducthai060501@gmail.com>
2024-12-31 08:58:57 +0200

    Add validation to signin route for email and password (with some errors)

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,26 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/api/users/singin', (req,res) => {
-    res.send("hi there ")
-})
+router.post(
+    "/api/users/singin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("You must supply a password"),
+  ],
+  (req: Request, res: Response) => {
+    console.log("singin route");
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
 
-export {router as singInRouter} ;
+  }
+);
+
+export { router as singInRouter };
