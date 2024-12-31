04591ba6eec19a1678268fc005f14be5f9273b52
vvduth <ducthai060501@gmail.com>
2024-12-31 08:19:59 +0200

    Add JWT support to signup route and update dependencies

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,21 +1,20 @@
-import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
-import { User } from '../models/user';
-import { RequestValidationError } from '../errors/request-validation-error';
-import { BadRequestError } from '../errors/bad-request-error';
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { User } from "../models/user";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { BadRequestError } from "../errors/bad-request-error";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 router.post(
-  '/api/users/signup',
+  "/api/users/signup",
   [
-    body('email')
-      .isEmail()
-      .withMessage('Email must be valid'),
-    body('password')
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
       .trim()
       .isLength({ min: 4, max: 20 })
-      .withMessage('Password must be between 4 and 20 characters')
+      .withMessage("Password must be between 4 and 20 characters"),
   ],
   async (req: Request, res: Response): Promise<any> => {
     const errors = validationResult(req);
@@ -29,11 +28,23 @@ router.post(
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-      throw new BadRequestError('Email in use');
+      throw new BadRequestError("Email in use");
     }
 
     const user = User.build({ email, password });
     await user.save();
+
+    // generate JWT
+    const userJtw = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      "asfd"
+    );
+
+    // store it on session object
+    req.session = { jwt: userJtw };
 
     res.status(201).send(user);
   }
