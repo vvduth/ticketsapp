15e76241f68ab1528ec34eefdf5f97173778356c
vvduth <ducthai060501@gmail.com>
2025-01-02 13:43:12 +0200

    Implement LandingPage component with server-side user data fetching using Axios

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  return <div>LandingPage</div>;
+};
 
-const Page = () => {
-  return (
-    <div>Page</div>
-  )
-}
-
-export default Page
+LandingPage.getInitialProps = async ({req}) => {
+  if (typeof window === "undefined") {
+    // we are on the server
+    // requests should be made to http://ingress-nginx.ingress-nginx...laksdjf
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: req.headers,
+      }
+    );
+    return data;
+  } else {
+    const { data } = await axios.get("/api/users/currentuser");
+    return data;
+  }
+  return {};
+};
+export default LandingPage;
