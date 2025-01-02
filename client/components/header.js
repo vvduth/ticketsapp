39a1d8cd06eb74beaf3d0deb0dc6a324e3a27841
vvduth <ducthai060501@gmail.com>
2025-01-02 14:53:16 +0200

    Implement Header component and integrate user authentication links; add signout functionality

---
+++ client/components/header.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Link from "next/link";
+
+const Header = ({ currentUser }) => {
+  const links = [
+    !currentUser && { label: "Sign Up", href: "/auth/signup" },
+    !currentUser && { label: "Sign In", href: "/auth/signin" },
+    currentUser && { label: "Sign Out", href: "/auth/signout" },
+  ]
+    .filter((linkConfig) => linkConfig)
+    .map(({ label, href }) => {
+      return (
+        <>
+          <li key={href} className="nav-item">
+            <Link href={href}>
+              <p className="nav-link text-white">{label}</p>
+            </Link>
+          </li>
+        </>
+      );
+    });
+  return (
+    <header className="navbar navbar-expand-lg navbar-light bg-success">
+      <Link href="/">
+        <p className="navbar-brand text-white">Ticket4real</p>
+      </Link>
+      <div className="d-flex justify-content-end">
+        <ul className="nav d-flex align-items-center">{links}</ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,25 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+import "bootstrap/dist/css/bootstrap.min.css";
+import buildClient from "../api/build-client";
+import Header from "../components/header";
+function AppComponent({ Component, pageProps, currentUser }) {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <h1>hello {currentUser ? currentUser.email : "no"}</h1>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp;
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/currentuser");
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+  return { pageProps, ...data };
+};
+
+export default AppComponent;
