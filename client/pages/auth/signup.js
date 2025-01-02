8c1fc82e159cf3cc0ec84c1eee64ab66f7436e24
vvduth <ducthai060501@gmail.com>
2025-01-02 11:55:11 +0200

    Add Axios for API requests in signup form and handle errors

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,18 +1,27 @@
 import React from "react";
 import { useState } from "react";
-
+import axios from "axios";
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    console.log(email, password);
+    try {
+      const response = await axios.post("/api/users/signup", {
+        email,
+        password,
+      });
+      console.log(response.data);
+    } catch (error) {
+      setErrors(error.response.data.errors);
+    }
   };
   return (
     <form onSubmit={onSubmit}>
       <h1>Sign up</h1>
-      <div className="form-group">
+      <div className="form-group" style={{ marginBottom: "15px" }}>
         <label htmlFor="email">Email address</label>
         <input
           value={email}
@@ -22,7 +31,7 @@ const Signup = () => {
           id="email"
         />
       </div>
-      <div className="form-group">
+      <div className="form-group" style={{ marginBottom: "15px" }}>
         <label htmlFor="password">Password</label>
         <input
           value={password}
@@ -32,6 +41,16 @@ const Signup = () => {
           id="password"
         />
       </div>
+      {errors.length > 0 && (
+        <div className="alert alert-danger" style={{ marginBottom: "15px" }}>
+          <h4>Nuh uh</h4>
+          <ul>
+            {errors.map((error) => (
+              <li key={error.message}>{error.message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
       <button type="submit" className="btn btn-primary">
         Sign up
       </button>
