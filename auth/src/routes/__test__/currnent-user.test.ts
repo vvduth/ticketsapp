4f1861aa23a3293b9ca74c0c212bea86f748a32c
vvduth <ducthai060501@gmail.com>
2025-01-02 11:09:38 +0200

    Add test for unauthenticated user response in current user endpoint

--- auth/src/routes/__test__/currnent-user.test.ts
+++ auth/src/routes/__test__/currnent-user.test.ts
@@ -16,3 +16,11 @@ it("responds with details about the current user", async () => {
         .expect(200);
     expect(response.body.currentUser.email).toEqual("test@test.com");
 });
+
+it("responses with null if not authenticated", async () => {
+    const response = await request(app)
+        .get("/api/users/currentuser")
+        .send()
+        .expect(200);
+    expect(response.body.currentUser).toEqual(null);    
+})
