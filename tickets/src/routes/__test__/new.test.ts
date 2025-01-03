f834c663a49b10af769feffdcae6f0db4b45f754
vvduth <ducthai060501@gmail.com>
2025-01-03 13:38:48 +0200

    Add Ticket model and update ticket creation tests

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,6 @@
 import request from "supertest";
 import { app } from "../../app";
-
+import { Ticket } from "../../models/ticket";
 it("has a route handler listing to /api/tickets for post request", async () => {
   const response = await request(app).post("/api/tickets").send({});
 
@@ -56,4 +56,16 @@ it("returns an error if an invalid price is provided", async () => {
     .expect(400);
 });
 
-it("creates a ticket with valid inputs", async () => {});
+it("creates a ticket with valid inputs", async () => {
+  // add in a check to make sure a ticket was saved
+  let tickets = await Ticket.find({});
+  
+
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+        title: "asdasd",
+        price: 20,
+    });
+});
