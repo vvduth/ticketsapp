48b998f165aca35e4fb6ae2bc32385dc1419a8ec
vvduth <ducthai060501@gmail.com>
2025-01-03 11:42:13 +0200

    Add ticket creation route and corresponding tests

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,10 +1,11 @@
-import express, { Application, Request, Response } from "express";
+import express from "express";
 import bodyParser from "body-parser";
 
 import cookieSession from "cookie-session";
 
 import { errorHandler, NotFoundError } from "@tikket4real/common";
 import "express-async-errors";
+import { createTicketRouter } from "./routes/new";
 
 const app = express();
 app.set("trust proxy", true);
@@ -15,7 +16,7 @@ app.use(bodyParser.json());
 app.use(cookieSession({ signed: false, secure: process.env.NODE_ENV !== "test" }));
 
 // Basic route
-
+app.use(createTicketRouter)
 
 app.all("*", () => {
   throw new NotFoundError();
---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,19 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+
+it("has a route handler listing to /api/tickets for post request", async () => {
+    const response = await request(app)
+        .post("/api/tickets")
+        .send({})
+
+    expect(response.status).not.toEqual(404)
+})
+
+it("can only be access if user is signed in", async () => {})
+
+it("returns an error if an invalid title is provided", async () => {})   
+
+it("returns an error if an invalid price is provided", async () => {})
+
+it("creates a ticket with valid inputs", async () => {})
