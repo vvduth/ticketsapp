0944f6461518f26942cffcd1488670af91176d40
vvduth <ducthai060501@gmail.com>
2025-01-06 14:55:11 +0200

    expoert ticket update event

--- common/src/index.ts
+++ common/src/index.ts
@@ -14,3 +14,4 @@ export * from "./events/base-listener";
 export * from "./events/base-publisher";
 export * from "./events/subjects";
 export * from "./events/ticket-created-event";
+export * from "./events/ticket-update-event";
