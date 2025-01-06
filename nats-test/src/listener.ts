2dd45aa53aff234cccb53a51dc18c31e706eda9e
vvduth <ducthai060501@gmail.com>
2025-01-06 12:49:56 +0200

    Implement base listener and ticket created listener for NATS messaging

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, { Message } from "node-nats-streaming";
+import nats, { Message, Stan } from "node-nats-streaming";
 import { randomBytes } from "crypto";
+import { TicketCreatedListener } from "./events/ticket-created-listener";
 // Create a client
 const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222",
@@ -12,26 +13,14 @@ stan.on("connect", () => {
   stan.on("close", () => {
     console.log("NATS connection closed!");
     process.exit();
-  })
-
-  // Subscribe to a subject
-  const options = stan.subscriptionOptions().setManualAckMode(true);
-  const subscription = stan.subscribe(
-    "ticket:created",
-    "orders-service-queue-group",
-    options
-  );
-
-  subscription.on("message", (msg: Message) => {
-    const data = msg.getData();
-
-    if (typeof data === "string") {
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-    }
-
-    msg.ack()
   });
+
+  new TicketCreatedListener(stan).listen();
+
 });
 
 process.on("SIGINT", () => stan.close()); // Handle interrupt signals
 process.on("SIGTERM", () => stan.close()); // Handle termination signals
+
+
+
