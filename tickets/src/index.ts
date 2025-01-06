e374ea3e857547733a902d017b56acf03b602064
vvduth <ducthai060501@gmail.com>
2025-01-06 15:44:48 +0200

    Add NATS configuration to deployment and validate environment variables in ticket service

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -11,16 +11,29 @@ const start = async () => {
   if (!process.env.MONGO_URI) {
     throw new Error("MONGO_URI must be defined");
   }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID must be defined");
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL must be defined");
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID must be defined");
+  }
   try {
-    await natsWrapper.connect("ticketing", "asdsad", "http://nats-srv:4222");
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
     natsWrapper.client.on("close", () => {
       console.log("NATS connection closed!");
       process.exit();
     });
     process.on("SIGINT", () => natsWrapper.client.close());
     process.on("SIGTERM", () => natsWrapper.client.close());
-    
-    await mongoose.connect(process.env.MONGO_URI,{});
+
+    await mongoose.connect(process.env.MONGO_URI, {});
     console.log("Connected to tickets MongoDB");
   } catch (error) {
     console.error(error);
