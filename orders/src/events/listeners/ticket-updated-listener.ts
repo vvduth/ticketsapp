1cb7bb50f255604c682f4bbe8c4db8e71db6185b
vvduth <ducthai060501@gmail.com>
2025-01-07 12:53:41 +0200

    Add ticket created and updated listeners with queue group name

---
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -0,0 +1,21 @@
+import { Message } from "node-nats-streaming";
+import { Subjects, Listener, TicketUpdatedEvent } from "@tikket4real/common";
+import { Ticket } from "../../models/ticket";
+import { queueGroupName } from "./queueGroupName";
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+  queueGroupName = queueGroupName;
+  async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
+    const ticket = await Ticket.findById(data);
+
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+
+    const { title, price } = data;
+    ticket.set({ title, price });
+    ticket.save();
+    msg.ack();
+  }
+}
--- orders/src/index.ts
+++ orders/src/index.ts
@@ -1,6 +1,8 @@
 import { app } from "./app";
 import mongoose from "mongoose";
 import { natsWrapper } from "./nats-wrapper";
+import { TicketCreatedListener } from "./events/listeners/ticket-created-listener";
+import { TicketUpdatedListener } from "./events/listeners/ticket-updated-listener";
 
 const port = 3000;
 
@@ -32,6 +34,9 @@ const start = async () => {
     });
     process.on("SIGINT", () => natsWrapper.client.close());
     process.on("SIGTERM", () => natsWrapper.client.close());
+
+    new TicketCreatedListener(natsWrapper.client).listen();
+    new TicketUpdatedListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI, {});
     console.log("Connected to orders MongoDB");
