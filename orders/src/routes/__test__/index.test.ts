99c33aee48eaa1760047c93e8a0c85f63045e3fa
vvduth <ducthai060501@gmail.com>
2025-01-07 10:17:43 +0200

    Add order fetching logic and tests for user-specific orders

---
+++ orders/src/routes/__test__/index.test.ts
@@ -0,0 +1,51 @@
+import request from "supertest";
+import { app } from "../../app";
+import { Order } from "../../models/order";
+import { Ticket } from "../../models/ticket";
+
+const buildTicket = async () => {
+  const ticket = Ticket.build({
+    title: "Ufc event",
+    price: 20,
+  });
+  await ticket.save();
+  return ticket;
+};
+
+it("fetches orders for a particular user", async () => {
+  // Create 3 tickets using ticket model
+  const ticketOne = await buildTicket();
+  const ticketTwo = await buildTicket();
+  const ticketThree = await buildTicket();
+
+  const userOne = global.signin();
+  const userTwo = global.signin();
+  // Create 1 order as User #1
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", userOne)
+    .send({ ticketId: ticketOne.id })
+    .expect(201);
+
+  // Create 2 orders as User #2
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", userTwo)
+    .send({ ticketId: ticketTwo.id })
+    .expect(201);
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", userTwo)
+    .send({ ticketId: ticketThree.id })
+    .expect(201);
+
+  // Make request to get orders for User #2
+  const res = await request(app)
+    .get("/api/orders")
+    .set("Cookie", userTwo)
+    .expect(200);
+
+  // Make sure we only got the orders for User #2
+  console.log(res.body);
+  expect(res.body.length).toEqual(2);
+});
--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -4,11 +4,13 @@ import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.get("/api/orders", requireAuth, (req: Request, res: Response) => {
-  const orders = Order.find({
+router.get("/api/orders", requireAuth,async (req: Request, res: Response) => {
+  const orders = await Order.find({
     userId: req.currentUser!.id,
   }).populate("ticket");
-  res.send({});
+  
+  
+  res.send(orders);
 });
 
 export { router as indexOrdersRouter };
