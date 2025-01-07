b2446f6eb3c94805e05713683bef2a03390d0b2f
vvduth <ducthai060501@gmail.com>
2025-01-07 10:32:52 +0200

    Add order fetching route and tests for user-specific orders

---
+++ orders/src/routes/__test__/show.test.ts
@@ -0,0 +1,52 @@
+import request from "supertest";
+import { app } from "../../app";
+import { Order } from "../../models/order";
+import { Ticket } from "../../models/ticket";
+
+it("fetches orders for a particular user", async () => {
+  // Create 1 ticket
+  const ticket = Ticket.build({
+    title: "BTS concert",
+    price: 20,
+  });
+  await ticket.save();
+  const user = global.signin();
+  // make a request to build an order with this ticket
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // make request to get orders
+  const { body: fetchedOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(200);
+
+  expect(fetchedOrder.id).toEqual(order.id);
+});
+
+it("returns an error if one user tries to fetch another user's order", async () => {
+  // Create 1 ticket
+  const ticket = Ticket.build({
+    title: "BTS concert",
+    price: 20,
+  });
+  await ticket.save();
+  const user = global.signin();
+  // make a request to build an order with this ticket
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // make request to get orders
+  await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(401);
+});
