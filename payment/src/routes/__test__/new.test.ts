92300cdd861e29390524beb86abd6e4b53e5107e
vvduth <ducthai060501@gmail.com>
2025-01-08 15:38:34 +0200

    Add Payment model and update tests for payment processing

--- payment/src/routes/__test__/new.test.ts
+++ payment/src/routes/__test__/new.test.ts
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 import { Order } from "../../models/order";
 import { stripe } from "../../stripe";
 import { OrderStatus } from "@tikket4real/common";
-jest.mock("../../stripe");
+
 // it("returns a 404 when purchasing an order that does not exist", async () => {
 //   const res = await request(app)
 //     .post("/api/payments")
@@ -60,13 +60,14 @@ jest.mock("../../stripe");
 //   expect(res.status).toEqual(400);
 // });
 
-it("returns a 204 with valid inputs", async () => {
+it("returns a 201 with valid inputs", async () => {
   const userId = new mongoose.Types.ObjectId().toHexString();
+  const price = Math.floor(Math.random() * 100000);
   const order = Order.build({
     id: new mongoose.Types.ObjectId().toHexString(),
     userId,
     version: 0,
-    price: 20,
+    price,
     status: OrderStatus.Created,
   });
   await order.save();
@@ -80,8 +81,15 @@ it("returns a 204 with valid inputs", async () => {
     })
     .expect(201);
 
-    const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
-    expect(chargeOptions.source).toEqual("tok_visa");
-    expect(chargeOptions.amount).toEqual(20 * 100);
-    expect(chargeOptions.currency).toEqual("usd");
+  // List 10 most recent charges
+  const stripeCharges = await stripe.charges.list({ limit: 50 });
+
+  // Find charge that matches the price of the order
+  const stripeCharge = stripeCharges.data.find((charge) => {
+    return charge.amount === price * 100;
+  });
+
+  expect(stripeCharge).toBeDefined();
+
+    
 });
--- payment/src/test/setup.ts
+++ payment/src/test/setup.ts
@@ -1,19 +1,22 @@
-import { MongoMemoryServer } from 'mongodb-memory-server';
-import mongoose from 'mongoose';
-import request from 'supertest';
-import { app } from '../app';
-import jwt from 'jsonwebtoken';
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import request from "supertest";
+import { app } from "../app";
+import jwt from "jsonwebtoken";
 
 declare global {
   var signin: (id?: string) => string[];
 }
 
-jest.mock('../nats-wrapper');
+jest.mock("../nats-wrapper");
+
+process.env.STRIPE_KEY =
+  "sk_test_51KPOjRCpGAJrQNJz1IY8ZxSFJZZZsklc33iHtbM8q39dhpoQuh1P6l9vZQQ9CKxhClmTxnBt6YFiEmpdjPtnANPi00631j3RMV";
 
 let mongo: any;
 beforeAll(async () => {
-  process.env.JWT_KEY = 'asdfasdf';
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+  process.env.JWT_KEY = "asdfasdf";
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
   const mongo = await MongoMemoryServer.create();
   const mongoUri = mongo.getUri();
@@ -43,7 +46,7 @@ global.signin = (id?: string) => {
   // Build a JWT payload.  { id, email }
   const payload = {
     id: id || new mongoose.Types.ObjectId().toHexString(),
-    email: 'test@test.com',
+    email: "test@test.com",
   };
 
   // Create the JWT!
@@ -56,7 +59,7 @@ global.signin = (id?: string) => {
   const sessionJSON = JSON.stringify(session);
 
   // Take JSON and encode it as base64
-  const base64 = Buffer.from(sessionJSON).toString('base64');
+  const base64 = Buffer.from(sessionJSON).toString("base64");
 
   // return a string thats the cookie with the encoded data
   return [`session=${base64}`];
