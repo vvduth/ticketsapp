48a082f2e50e3f432f78f579c002974452088d35
vvduth <ducthai060501@gmail.com>
2025-01-08 12:40:49 +0200

    Add payment service with Docker configuration, NATS wrapper, and MongoDB setup

---
+++ payment/src/app.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+import cookieSession from "cookie-session";
+
+import { errorHandler, NotFoundError, currentUser } from "@tikket4real/common";
+import "express-async-errors";
+
+
+const app = express();
+app.set("trust proxy", true);
+
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+app.use(cookieSession({ signed: false, secure: process.env.NODE_ENV !== "test" }));
+app.use(currentUser);
+
+
+
+app.all("*", () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler as express.ErrorRequestHandler);
+
+export {app}
